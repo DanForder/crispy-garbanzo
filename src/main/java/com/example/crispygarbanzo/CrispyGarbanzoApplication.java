8a2841ad325fe971a66ec0dfa5f608f5a2a9607c
Dan Forder <danfrdr@gmail.com>
2022-01-07 22:13:23 +0000

    initial commit

---
+++ src/main/java/com/example/crispygarbanzo/CrispyGarbanzoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crispygarbanzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispyGarbanzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrispyGarbanzoApplication.class, args);
+	}
+
+}
